ac6ad11c9aa701ab10a63117784665ef0b9b5c0a
mostafa-re <53138478+mostafa-re@users.noreply.github.com>
2024-03-13 19:36:08 +0330

    Recreate database

--- cart/apps.py
+++ cart/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class CartConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'cart'
--- eduspace/apps.py
+++ eduspace/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class EduspaceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'eduspace'
